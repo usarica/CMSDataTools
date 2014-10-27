e20529f2f5b18c5400da20aaf47b5166caf43843
ijanderso <ianderso@pha.jhu.edu>
2014-10-27 16:15:27 -0400

    Modified ZZ4l to test fLQ, added histograms for fLQ, pushed files for Djet splitting

--- data/ZZ4l_125_6_Samples.h
+++ data/ZZ4l_125_6_Samples.h
@@ -129,11 +129,11 @@ TString sample_BackgroundFile[kNumBkg]={
 };
 //Directories
 
-TString user_dir="./161014/";
+TString user_dir="./271014/";
 //TString user_dir="/afs/cern.ch/work/u/usarica/WidthAnalysis/PostICHEP/";
 TString user_gg2VV_location="/scratch0/hep/ianderso/CJLST/ReprocessedTrees/HZZ4l-125p6-FullAnalysis/";
 //TString user_gg2VV_location="/afs/cern.ch/work/u/usarica/HZZ4l-125p6-FullAnalysis/";
-TString user_TemplateswithTrees_dir="/scratch0/hep/ianderso/TemplatesForCombine/161014/";
+TString user_TemplateswithTrees_dir="/scratch0/hep/ianderso/TemplatesForCombine/271014/";
 //TString user_TemplateswithTrees_dir="/afs/cern.ch/work/u/usarica/WidthAnalysis/PostICHEP/";
 TString user_folder[5]={
 	"4mu",
@@ -153,6 +153,6 @@ TString strAnomalousType[kCouplings] = {
 	"fLQAdded"
 };
 int const nAnomalousCouplingTemplates[kCouplings][2] = { // 0: ggHVV, 1: VVHVV
-	[1,1],
-	[3,5]
+	{1,1},
+	{3,5}
 };
---
+++ getOnShellWeightsforfLQ.C
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include "TChain.h"
+#include "TString.h"
+#include "TSpline.h"
+#include "TSystem.h"
+#include "TGraph.h"
+#include "TGraphAsymmErrors.h"
+#include "TH1F.h"
+#include "TH1D.h"
+#include "TH2F.h"
+#include "TH3F.h"
+#include "TF1.h"
+#include "./data/ZZ4l_125_6_Samples.h"
+#include "./data/FitDimensionsList.h"
+
+using namespace std;
+
+void getOnShellWeightsforfLQ_one(int erg_tev);
+
+void getOnShellWeightsforfLQ(){
+	getOnShellWeightsforfLQ_one(7);
+	getOnShellWeightsforfLQ_one(8);
+}
+
+void getOnShellWeightsforfLQ_one(int erg_tev){
+	TString inputdir = user_gg2VV_location;
+	TString erg_dir;
+	erg_dir.Form("LHC_%iTeV/",erg_tev);
+	TString erg_name;
+	erg_name.Form("%i",erg_tev);
+	float Hmass=125.6;
+	TString ggH_125p6 = "HZZ4lTree_powheg15jhuGenV3-0PMH125.6_Generated.root";
+	TString VBF_125p6 = "VBFHiggs0PToZZTo4L_M-125p6_" + erg_name + "TeV-JHUGenV4_false.root";
+
+	TChain* ggH = new TChain("GenTree");
+	ggH->Add(inputdir + erg_dir + "GenSignal/" + ggH_125p6);
+	TChain* VBF = new TChain("SelectedTree");
+	VBF->Add(inputdir + erg_dir + "GenSignal/" + VBF_125p6);
+
+	float mZZ;
+	ggH->SetBranchAddress("GenZZMass",&mZZ);
+	VBF->SetBranchAddress("ZZMass",&mZZ);
+
+	double ggH_0=1.;
+	double ggH_1=0.;
+	double ggH_2=0.;
+	double VBF_0=1.;
+	double VBF_1=0.;
+	double VBF_2=0.;
+	double VBF_3=0.;
+	double VBF_4=0.;
+
+	for(int iEvt=0;iEvt<ggH->GetEntries();++iEvt){
+		ggH->GetEntry(iEvt);
+		ggH_1+=(double)(pow(mZZ,2.)/pow(Hmass,2.));
+		ggH_2+=(double)pow(pow(mZZ,2.)/pow(Hmass,2.),2.);
+	}
+	ggH_1/=ggH->GetEntries();
+	ggH_2/=ggH->GetEntries();
+
+	for(int iEvt=0;iEvt<VBF->GetEntries();++iEvt){
+		VBF->GetEntry(iEvt);
+		VBF_1+=(double)(pow(mZZ,2.)/pow(Hmass,2.));
+		VBF_2+=(double)pow(pow(mZZ,2.)/pow(Hmass,2.),2.);
+		VBF_3+=(double)pow(pow(mZZ,2.)/pow(Hmass,2.),3.);
+		VBF_4+=(double)pow(pow(mZZ,2.)/pow(Hmass,2.),4.);
+	}
+	VBF_1/=VBF->GetEntries();
+	VBF_2/=VBF->GetEntries();
+	VBF_3/=VBF->GetEntries();
+	VBF_4/=VBF->GetEntries();
+
+	cout<<erg_tev<<endl;
+	cout<<"ggH"<<endl;
+	cout<<setprecision(14)<<ggH_0<<endl;
+	cout<<setprecision(14)<<ggH_1<<endl;
+	cout<<setprecision(14)<<ggH_2<<endl;
+	cout<<"VBF"<<endl;
+	cout<<setprecision(14)<<VBF_0<<endl;
+	cout<<setprecision(14)<<VBF_1<<endl;
+	cout<<setprecision(14)<<VBF_2<<endl;
+	cout<<setprecision(14)<<VBF_3<<endl;
+	cout<<setprecision(14)<<VBF_4<<endl;
+
+	TH1D* ggH_ratios = new TH1D("ggH_ratios","ggH_ratios",3,0.,3g.);
+	TH1D* VBF_ratios = new TH1D("VBF_ratios","VBF_ratios",5,0.,5.);
+	ggH_ratios->Fill(0.,ggH_0);
+	ggH_ratios->Fill(1.,ggH_1);
+	ggH_ratios->Fill(2.,ggH_2);
+	VBF_ratios->Fill(0.,VBF_0);
+	VBF_ratios->Fill(1.,VBF_1);
+	VBF_ratios->Fill(2.,VBF_2);
+	VBF_ratios->Fill(3.,VBF_3);
+	VBF_ratios->Fill(4.,VBF_4);
+
+	TString outputname = "./data/m4l_ratios_" + erg_name + "TeV.root";  
+	TFile* output = new TFile(outputname,"recreate");
+	output->WriteTObject(ggH_ratios);
+	output->WriteTObject(VBF_ratios);
+	output->Close();
+}

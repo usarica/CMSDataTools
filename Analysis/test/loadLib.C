6c742864bff74db73e682b5399ddb7f9c5388345
Ulascan Sarica <ulascan90@gmail.com>
2017-10-10 14:04:29 -0400

    Depend on MELA

--- Analysis/test/loadLib.C
+++ Analysis/test/loadLib.C
@@ -3,5 +3,6 @@
   gSystem->AddIncludePath("-I$CMSSW_BASE/interface/");
   gSystem->AddIncludePath("-I$CMSSW_BASE/src/");
   gSystem->AddIncludePath("-I$CMSSW_BASE/src/HiggsWidth_PostICHEP/Analysis/interface/");
+  gSystem->Load("$CMSSW_BASE/src/ZZMatrixElement/MELA/test/loadMELA.C");
   gSystem->Load("libHiggsWidth_PostICHEPAnalysis.so");
 }

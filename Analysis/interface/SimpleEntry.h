c47f469e0919ea4e368ad06b01f5fb2b0c8abdf6
Ulascan Sarica <ulascan90@gmail.com>
2017-10-27 01:30:49 +0200

    Restore SimpleEntry

--- Analysis/interface/SimpleEntry.h
+++ Analysis/interface/SimpleEntry.h
@@ -14,15 +14,15 @@ struct SimpleEntry{
   float weight;
 
   std::unordered_map<TString, bool> namedbools;
-  std::unordered_map<TString, unsigned int> nameduints;
   std::unordered_map<TString, short> namedshorts;
+  std::unordered_map<TString, unsigned int> nameduints;
   std::unordered_map<TString, int> namedints;
   std::unordered_map<TString, float> namedfloats;
   std::unordered_map<TString, double> nameddoubles;
 
   std::unordered_map<TString, std::vector<bool>> namedVbools;
-  std::unordered_map<TString, std::vector<unsigned int>> namedVuints;
   std::unordered_map<TString, std::vector<short>> namedVshorts;
+  std::unordered_map<TString, std::vector<unsigned int>> namedVuints;
   std::unordered_map<TString, std::vector<int>> namedVints;
   std::unordered_map<TString, std::vector<float>> namedVfloats;
   std::unordered_map<TString, std::vector<double>> namedVdoubles;
@@ -37,6 +37,10 @@ struct SimpleEntry{
   bool operator >= (const SimpleEntry& other)const;
   bool operator < (const SimpleEntry& other)const;
   bool operator <= (const SimpleEntry& other)const;
+
+
+  template<typename T> std::unordered_map<TString, T> const& getNamedMap() const;
+  template<typename T> std::unordered_map<TString, T>& getNamedMap();
 
   template<typename T> void setNamedVal(TString strname, T const& val);
   template<typename T> void getNamedVal(TString strname, T& val) const;
--- Analysis/interface/SimpleEntry.hpp
+++ Analysis/interface/SimpleEntry.hpp
@@ -4,127 +4,65 @@
 #include "SimpleEntry.h"
 
 
-template<> void SimpleEntry::setNamedVal<bool>(TString strname, bool const& val){
-  typedef bool V;
-  std::unordered_map<TString, V>::iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedbools, it)) it->second = val;
-}
-template<> void SimpleEntry::getNamedVal<bool>(TString strname, bool& val) const{
-  typedef bool V;
-  std::unordered_map<TString, V>::const_iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedbools, it)) val = it->second;
-}
-template<> void SimpleEntry::setNamedVal<short>(TString strname, short const& val){
-  typedef short V;
-  std::unordered_map<TString, V>::iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedshorts, it)) it->second = val;
-}
-template<> void SimpleEntry::getNamedVal<short>(TString strname, short& val) const{
-  typedef short V;
-  std::unordered_map<TString, V>::const_iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedshorts, it)) val = it->second;
-}
-template<> void SimpleEntry::setNamedVal<unsigned int>(TString strname, unsigned int const& val){
-  typedef unsigned int V;
-  std::unordered_map<TString, V>::iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, nameduints, it)) it->second = val;
-}
-template<> void SimpleEntry::getNamedVal<unsigned int>(TString strname, unsigned int& val) const{
-  typedef unsigned int V;
-  std::unordered_map<TString, V>::const_iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, nameduints, it)) val = it->second;
-}
-template<> void SimpleEntry::setNamedVal<int>(TString strname, int const& val){
-  typedef int V;
-  std::unordered_map<TString, V>::iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedints, it)) it->second = val;
-}
-template<> void SimpleEntry::getNamedVal<int>(TString strname, int& val) const{
-  typedef int V;
-  std::unordered_map<TString, V>::const_iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedints, it)) val = it->second;
-}
-template<> void SimpleEntry::setNamedVal<float>(TString strname, float const& val){
-  typedef float V;
-  std::unordered_map<TString, V>::iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedfloats, it)) it->second = val;
-}
-template<> void SimpleEntry::getNamedVal<float>(TString strname, float& val) const{
-  typedef float V;
-  std::unordered_map<TString, V>::const_iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedfloats, it)) val = it->second;
-}
-template<> void SimpleEntry::setNamedVal<double>(TString strname, double const& val){
-  typedef double V;
-  std::unordered_map<TString, V>::iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, nameddoubles, it)) it->second = val;
-}
-template<> void SimpleEntry::getNamedVal<double>(TString strname, double& val) const{
-  typedef double V;
-  std::unordered_map<TString, V>::const_iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, nameddoubles, it)) val = it->second;
-}
+template<> std::unordered_map<TString, bool> const& SimpleEntry::getNamedMap<bool>() const{ return namedbools; }
+template<> std::unordered_map<TString, short> const& SimpleEntry::getNamedMap<short>() const{ return namedshorts; }
+template<> std::unordered_map<TString, unsigned int> const& SimpleEntry::getNamedMap<unsigned int>() const{ return nameduints; }
+template<> std::unordered_map<TString, int> const& SimpleEntry::getNamedMap<int>() const{ return namedints; }
+template<> std::unordered_map<TString, float> const& SimpleEntry::getNamedMap<float>() const{ return namedfloats; }
+template<> std::unordered_map<TString, double> const& SimpleEntry::getNamedMap<double>() const{ return nameddoubles; }
+template<> std::unordered_map<TString, std::vector<bool>> const& SimpleEntry::getNamedMap<std::vector<bool>>() const{ return namedVbools; }
+template<> std::unordered_map<TString, std::vector<short>> const& SimpleEntry::getNamedMap<std::vector<short>>() const{ return namedVshorts; }
+template<> std::unordered_map<TString, std::vector<unsigned int>> const& SimpleEntry::getNamedMap<std::vector<unsigned int>>() const{ return namedVuints; }
+template<> std::unordered_map<TString, std::vector<int>> const& SimpleEntry::getNamedMap<std::vector<int>>() const{ return namedVints; }
+template<> std::unordered_map<TString, std::vector<float>> const& SimpleEntry::getNamedMap<std::vector<float>>() const{ return namedVfloats; }
+template<> std::unordered_map<TString, std::vector<double>> const& SimpleEntry::getNamedMap<std::vector<double>>() const{ return namedVdoubles; }
 
-template<> void SimpleEntry::setNamedVal<std::vector<bool>>(TString strname, std::vector<bool> const& val){
-  typedef std::vector<bool> V;
-  std::unordered_map<TString, V>::iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedVbools, it)) it->second = val;
+template<> std::unordered_map<TString, bool>& SimpleEntry::getNamedMap<bool>(){ return namedbools; }
+template<> std::unordered_map<TString, short>& SimpleEntry::getNamedMap<short>(){ return namedshorts; }
+template<> std::unordered_map<TString, unsigned int>& SimpleEntry::getNamedMap<unsigned int>(){ return nameduints; }
+template<> std::unordered_map<TString, int>& SimpleEntry::getNamedMap<int>(){ return namedints; }
+template<> std::unordered_map<TString, float>& SimpleEntry::getNamedMap<float>(){ return namedfloats; }
+template<> std::unordered_map<TString, double>& SimpleEntry::getNamedMap<double>(){ return nameddoubles; }
+template<> std::unordered_map<TString, std::vector<bool>>& SimpleEntry::getNamedMap<std::vector<bool>>(){ return namedVbools; }
+template<> std::unordered_map<TString, std::vector<short>>& SimpleEntry::getNamedMap<std::vector<short>>(){ return namedVshorts; }
+template<> std::unordered_map<TString, std::vector<unsigned int>>& SimpleEntry::getNamedMap<std::vector<unsigned int>>(){ return namedVuints; }
+template<> std::unordered_map<TString, std::vector<int>>& SimpleEntry::getNamedMap<std::vector<int>>(){ return namedVints; }
+template<> std::unordered_map<TString, std::vector<float>>& SimpleEntry::getNamedMap<std::vector<float>>(){ return namedVfloats; }
+template<> std::unordered_map<TString, std::vector<double>>& SimpleEntry::getNamedMap<std::vector<double>>(){ return namedVdoubles; }
+
+template<typename T> void SimpleEntry::setNamedVal(TString strname, T const& val){
+  std::unordered_map<TString, T>& theMap = this->getNamedMap<T>();
+  theMap[strname] = val;
 }
-template<> void SimpleEntry::getNamedVal<std::vector<bool>>(TString strname, std::vector<bool>& val) const{
-  typedef std::vector<bool> V;
-  std::unordered_map<TString, V>::const_iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedVbools, it)) val = it->second;
+template void SimpleEntry::setNamedVal<bool>(TString strname, bool const& val);
+template void SimpleEntry::setNamedVal<short>(TString strname, short const& val);
+template void SimpleEntry::setNamedVal<unsigned int>(TString strname, unsigned int const& val);
+template void SimpleEntry::setNamedVal<int>(TString strname, int const& val);
+template void SimpleEntry::setNamedVal<float>(TString strname, float const& val);
+template void SimpleEntry::setNamedVal<double>(TString strname, double const& val);
+template void SimpleEntry::setNamedVal<std::vector<bool>>(TString strname, std::vector<bool> const& val);
+template void SimpleEntry::setNamedVal<std::vector<short>>(TString strname, std::vector<short> const& val);
+template void SimpleEntry::setNamedVal<std::vector<unsigned int>>(TString strname, std::vector<unsigned int> const& val);
+template void SimpleEntry::setNamedVal<std::vector<int>>(TString strname, std::vector<int> const& val);
+template void SimpleEntry::setNamedVal<std::vector<float>>(TString strname, std::vector<float> const& val);
+template void SimpleEntry::setNamedVal<std::vector<double>>(TString strname, std::vector<double> const& val);
+
+template<typename T> void SimpleEntry::getNamedVal(TString strname, T& val) const{
+  typename std::unordered_map<TString, T>::const_iterator it;
+  if (HelperFunctions::getUnorderedMapIterator<T>(strname, this->getNamedMap<T>(), it)) val = it->second;
 }
-template<> void SimpleEntry::setNamedVal<std::vector<short>>(TString strname, std::vector<short> const& val){
-  typedef std::vector<short> V;
-  std::unordered_map<TString, V>::iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedVshorts, it)) it->second = val;
-}
-template<> void SimpleEntry::getNamedVal<std::vector<short>>(TString strname, std::vector<short>& val) const{
-  typedef std::vector<short> V;
-  std::unordered_map<TString, V>::const_iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedVshorts, it)) val = it->second;
-}
-template<> void SimpleEntry::setNamedVal<std::vector<unsigned int>>(TString strname, std::vector<unsigned int> const& val){
-  typedef std::vector<unsigned int> V;
-  std::unordered_map<TString, V>::iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedVuints, it)) it->second = val;
-}
-template<> void SimpleEntry::getNamedVal<std::vector<unsigned int>>(TString strname, std::vector<unsigned int>& val) const{
-  typedef std::vector<unsigned int> V;
-  std::unordered_map<TString, V>::const_iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedVuints, it)) val = it->second;
-}
-template<> void SimpleEntry::setNamedVal<std::vector<int>>(TString strname, std::vector<int> const& val){
-  typedef std::vector<int> V;
-  std::unordered_map<TString, V>::iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedVints, it)) it->second = val;
-}
-template<> void SimpleEntry::getNamedVal<std::vector<int>>(TString strname, std::vector<int>& val) const{
-  typedef std::vector<int> V;
-  std::unordered_map<TString, V>::const_iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedVints, it)) val = it->second;
-}
-template<> void SimpleEntry::setNamedVal<std::vector<float>>(TString strname, std::vector<float> const& val){
-  typedef std::vector<float> V;
-  std::unordered_map<TString, V>::iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedVfloats, it)) it->second = val;
-}
-template<> void SimpleEntry::getNamedVal<std::vector<float>>(TString strname, std::vector<float>& val) const{
-  typedef std::vector<float> V;
-  std::unordered_map<TString, V>::const_iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedVfloats, it)) val = it->second;
-}
-template<> void SimpleEntry::setNamedVal<std::vector<double>>(TString strname, std::vector<double> const& val){
-  typedef std::vector<double> V;
-  std::unordered_map<TString, V>::iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedVdoubles, it)) it->second = val;
-}
-template<> void SimpleEntry::getNamedVal<std::vector<double>>(TString strname, std::vector<double>& val) const{
-  typedef std::vector<double> V;
-  std::unordered_map<TString, V>::const_iterator it;
-  if (HelperFunctions::getUnorderedMapIterator<V>(strname, namedVdoubles, it)) val = it->second;
-}
+template void SimpleEntry::getNamedVal<bool>(TString strname, bool& val) const;
+template void SimpleEntry::getNamedVal<short>(TString strname, short& val) const;
+template void SimpleEntry::getNamedVal<unsigned int>(TString strname, unsigned int& val) const;
+template void SimpleEntry::getNamedVal<int>(TString strname, int& val) const;
+template void SimpleEntry::getNamedVal<float>(TString strname, float& val) const;
+template void SimpleEntry::getNamedVal<double>(TString strname, double& val) const;
+template void SimpleEntry::getNamedVal<std::vector<bool>>(TString strname, std::vector<bool>& val) const;
+template void SimpleEntry::getNamedVal<std::vector<short>>(TString strname, std::vector<short>& val) const;
+template void SimpleEntry::getNamedVal<std::vector<unsigned int>>(TString strname, std::vector<unsigned int>& val) const;
+template void SimpleEntry::getNamedVal<std::vector<int>>(TString strname, std::vector<int>& val) const;
+template void SimpleEntry::getNamedVal<std::vector<float>>(TString strname, std::vector<float>& val) const;
+template void SimpleEntry::getNamedVal<std::vector<double>>(TString strname, std::vector<double>& val) const;
 
 
 #endif

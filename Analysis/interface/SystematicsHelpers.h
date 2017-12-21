c9edefa0573fedceb5632e6568abc444a76e2163
Ulascan Sarica <ulascan90@gmail.com>
2017-12-21 12:55:58 -0500

    Add sf unc

--- Analysis/interface/SystematicsHelpers.h
+++ Analysis/interface/SystematicsHelpers.h
@@ -30,20 +30,22 @@ namespace SystematicsHelpers{
 
   class PerLeptonSystematic : public SystematicsClass{
   protected:
-    float(*rule)(std::vector<short>* const&, std::vector<std::vector<float>*> const&);
-    TString strLepId;
+    std::pair<float, float> (*rule)(short const& Z1Flav, short const& Z2Flav, std::vector<std::vector<float>*> const&);
     std::vector<TString> strVars;
+    bool doUp;
     std::unordered_map<CJLSTTree*,
       std::pair<
-      std::vector<short>*, std::vector<std::vector<float>*>
+      std::vector<short*>, std::vector<std::vector<float>*>
       >
     > componentRefs;
   public:
-    PerLeptonSystematic(const TString inStrLepId, const std::vector<TString>& inStrVars, float(*infcn)(std::vector<short>* const&, std::vector<std::vector<float>*> const&));
+    PerLeptonSystematic(const std::vector<TString>& inStrVars, std::pair<float, float> (*infcn)(short const& Z1Flav, short const& Z2Flav, std::vector<std::vector<float>*> const&), bool doUp_);
     virtual ~PerLeptonSystematic(){}
     virtual float eval(CJLSTTree* theTree) const;
     virtual void setup(CJLSTTree* theTree);
   };
+
+  std::pair<float, float> getLeptonSFSystematic(short const& Z1Flav, short const& Z2Flav, std::vector<std::vector<float>*> const& LepVars);
 
 
 }
--- Analysis/src/SystematicsHelpers.cc
+++ Analysis/src/SystematicsHelpers.cc
@@ -28,8 +28,8 @@ float SystematicsHelpers::getNormalizedSystematic(CJLSTTree* theTree, const std:
 
 
 SystematicsHelpers::PerLeptonSystematic::PerLeptonSystematic(
-  const TString inStrLepId, const std::vector<TString>& inStrVars, float(*infcn)(std::vector<short>* const&, std::vector<std::vector<float>*> const&)
-) : rule(infcn), strLepId(inStrLepId), strVars(inStrVars)
+  const std::vector<TString>& inStrVars, std::pair<float, float> (*infcn)(short const& Z1Flav, short const& Z2Flav, std::vector<std::vector<float>*> const&), bool doUp_
+) : rule(infcn), strVars(inStrVars), doUp(doUp_)
 {}
 float SystematicsHelpers::PerLeptonSystematic::eval(CJLSTTree* theTree) const{
   auto it = componentRefs.find(theTree);
@@ -37,18 +37,64 @@ float SystematicsHelpers::PerLeptonSystematic::eval(CJLSTTree* theTree) const{
     MELAerr << "PerLeptonSystematic::eval: Could not find the variables for tree " << theTree->sampleIdentifier << ". Call PerLeptonSystematic::setup first!" << endl;
     return 0;
   }
-  return rule(it->second.first, it->second.second);
+  std::pair<float, float> res = rule(*(it->second.first.at(0)), *(it->second.first.at(1)), it->second.second);
+  return (doUp ? res.second : res.first);
 }
 void SystematicsHelpers::PerLeptonSystematic::setup(CJLSTTree* theTree){
-  if (!theTree || strVars.empty() || strLepId=="") return;
-  std::vector<short>* refId=nullptr; theTree->getValRef(strLepId, refId);
+  if (!theTree || strVars.empty()) return;
+  std::vector<short*> refId; refId.assign(2, nullptr);
+  theTree->getValRef("Z1Flav", refId.at(0));
+  theTree->getValRef("Z2Flav", refId.at(1));
   std::vector<std::vector<float>*> refVar;
-  for(TString const& s:strVars){
+  for (TString const& s:strVars){
     vector<float>* v = nullptr;
     theTree->getValRef(s, v);
     if (!v) MELAerr << "PerLeptonSystematic::setup: Could not get the reference for " << s << endl;
     refVar.push_back(v);
   }
-  if (refId && !refVar.empty()) componentRefs[theTree]=std::pair<std::vector<short>*, std::vector<std::vector<float>*>>(refId, refVar);
+  if (!refVar.empty()) componentRefs[theTree]=std::pair<std::vector<short*>, std::vector<std::vector<float>*>>(refId, refVar);
 }
 
+std::pair<float, float> SystematicsHelpers::getLeptonSFSystematic(short const& Z1Flav, short const& Z2Flav, std::vector<std::vector<float>*> const& LepVars){
+  std::pair<float, float> res(1, 1);
+  float const& lep4pt = LepVars.at(0)->back();
+  for (unsigned int ilep=0; ilep<LepVars.at(0)->size(); ilep++){
+    res.first *= (1.-LepVars.at(2)->at(ilep)/LepVars.at(1)->at(ilep))*(1.-LepVars.at(4)->at(ilep)/LepVars.at(3)->at(ilep));
+    res.second *= (1.+LepVars.at(2)->at(ilep)/LepVars.at(1)->at(ilep))*(1.+LepVars.at(4)->at(ilep)/LepVars.at(3)->at(ilep));
+  }
+  if (std::abs(Z1Flav)==169 && std::abs(Z2Flav)==169){
+    if (lep4pt<7.){
+      res.first *= 1.-0.032;
+      res.second *= 1.+0.001;
+    }
+    else{
+      res.first *= 1.-0.015;
+      res.second *= 1.+0.001;
+    }
+  }
+  else if (std::abs(Z1Flav)==121 && std::abs(Z2Flav)==121){
+    if (lep4pt<12.){
+      res.first *= 1.-0.011;
+      res.second *= 1.+0.001;
+    }
+    else{
+      res.first *= 1.-0.010;
+      res.second *= 1.+0.005;
+    }
+  }
+  else{
+    if (lep4pt<7.){
+      res.first *= 1.-0.080;
+      res.second *= 1.+0.005;
+    }
+    else if (lep4pt<12.){
+      res.first *= 1.-0.032;
+      res.second *= 1.+0.005;
+    }
+    else{
+      res.first *= 1.-0.010;
+      res.second *= 1.+0.001;
+    }
+  }
+  return res;
+}

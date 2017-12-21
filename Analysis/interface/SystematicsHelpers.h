b359ac80d43920d803973acb52c42c449221daaa
Ulascan Sarica <ulascan90@gmail.com>
2017-12-21 04:33:26 -0500

    Add systematics class 2

--- Analysis/interface/SystematicsHelpers.h
+++ Analysis/interface/SystematicsHelpers.h
@@ -6,20 +6,42 @@
 
 namespace SystematicsHelpers{
 
-  class ProcessSystematic{
+  class SystematicsClass{
+  public:
+    SystematicsClass(){}
+    virtual float eval(CJLSTTree* theTree) const=0;
+  };
+
+  class YieldSystematic : public SystematicsClass{
   protected:
     float(*rule)(CJLSTTree*, const std::vector<ReweightingBuilder*>&);
-
     std::vector<ReweightingBuilder*> evaluators;
 
   public:
-    ProcessSystematic(const std::vector<ReweightingBuilder*>& inEvaluators, float(*infcn)(CJLSTTree*, const std::vector<ReweightingBuilder*>&));
+    YieldSystematic(const std::vector<ReweightingBuilder*>& inEvaluators, float(*infcn)(CJLSTTree*, const std::vector<ReweightingBuilder*>&));
     virtual float eval(CJLSTTree* theTree) const;
-
   };
 
   float getRawSystematic(CJLSTTree* theTree, const std::vector<ReweightingBuilder*>& builders);
   float getNormalizedSystematic(CJLSTTree* theTree, const std::vector<ReweightingBuilder*>& builders);
+
+
+  class PerLeptonSystematic : public SystematicsClass{
+  protected:
+    float(*rule)(std::vector<short>* const&, std::vector<std::vector<float>*> const&);
+    TString strLepId;
+    std::vector<TString> strVars;
+    std::unordered_map<CJLSTTree*,
+      std::pair<
+      std::vector<short>*, std::vector<std::vector<float>*>
+      >
+    > componentRefs;
+  public:
+    PerLeptonSystematic(const TString inStrLepId, const std::vector<TString>& inStrVars, float(*infcn)(std::vector<short>* const&, std::vector<std::vector<float>*> const&));
+    virtual float eval(CJLSTTree* theTree) const;
+    virtual void setup(CJLSTTree* theTree);
+  };
+
 
 }
 
--- Analysis/src/SystematicsHelpers.cc
+++ Analysis/src/SystematicsHelpers.cc
@@ -1,8 +1,13 @@
 #include "SystematicsHelpers.h"
+#include "MELAStreamHelpers.hh"
 
 
-SystematicsHelpers::ProcessSystematic::ProcessSystematic(const std::vector<ReweightingBuilder*>& inEvaluators, float(*infcn)(CJLSTTree*, const std::vector<ReweightingBuilder*>&)) : rule(infcn), evaluators(inEvaluators) {}
-float SystematicsHelpers::ProcessSystematic::eval(CJLSTTree* theTree) const{
+using namespace std;
+using namespace MELAStreamHelpers;
+
+
+SystematicsHelpers::YieldSystematic::YieldSystematic(const std::vector<ReweightingBuilder*>& inEvaluators, float(*infcn)(CJLSTTree*, const std::vector<ReweightingBuilder*>&)) : rule(infcn), evaluators(inEvaluators) {}
+float SystematicsHelpers::YieldSystematic::eval(CJLSTTree* theTree) const{
   float weight=0;
   if (rule) weight=rule(theTree, evaluators);
   return weight;
@@ -20,3 +25,30 @@ float SystematicsHelpers::getNormalizedSystematic(CJLSTTree* theTree, const std:
   if (sumWeights!=0.) weight = builders.at(0)->getPostThresholdWeight(theTree)*builders.at(1)->getSumPostThresholdWeights(theTree)/sumWeights;
   return weight;
 }
+
+
+SystematicsHelpers::PerLeptonSystematic::PerLeptonSystematic(
+  const TString inStrLepId, const std::vector<TString>& inStrVars, float(*infcn)(std::vector<short>* const&, std::vector<std::vector<float>*> const&)
+) : rule(infcn), strLepId(inStrLepId), strVars(inStrVars)
+{}
+float SystematicsHelpers::PerLeptonSystematic::eval(CJLSTTree* theTree) const{
+  auto it = componentRefs.find(theTree);
+  if (it==componentRefs.cend()){
+    MELAerr << "PerLeptonSystematic::eval: Could not find the variables for tree " << theTree->sampleIdentifier << ". Call PerLeptonSystematic::setup first!" << endl;
+    return 0;
+  }
+  return rule(it->second.first, it->second.second);
+}
+void SystematicsHelpers::PerLeptonSystematic::setup(CJLSTTree* theTree){
+  if (!theTree || strVars.empty() || strLepId=="") return;
+  std::vector<short>* refId=nullptr; theTree->getValRef(strLepId, refId);
+  std::vector<std::vector<float>*> refVar;
+  for(TString const& s:strVars){
+    vector<float>* v = nullptr;
+    theTree->getValRef(s, v);
+    if (!v) MELAerr << "PerLeptonSystematic::setup: Could not get the reference for " << s << endl;
+    refVar.push_back(v);
+  }
+  if (refId && !refVar.empty()) componentRefs[theTree]=std::pair<std::vector<short>*, std::vector<std::vector<float>*>>(refId, refVar);
+}
+

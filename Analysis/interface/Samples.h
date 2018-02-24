5e75d685bc5b88880d65be63d6771c4a4e059733
Ulascan Sarica <ulascan90@gmail.com>
2018-02-24 08:13:00 -0500

    Revert 2017 samples

--- Analysis/interface/Samples.h
+++ Analysis/interface/Samples.h
@@ -6,10 +6,10 @@
 
 // LHC sqrts and data period
 const unsigned int theSqrts = 13;
-const TString theDataPeriod = "2017";
+const TString theDataPeriod = "2016";
 
 // CJLST samples directory
-const unsigned int CJLSTversion = 180218;
+const unsigned int CJLSTversion = 180212;
 const TString CJLSTdate = std::to_string(CJLSTversion);
 const TString CJLSTrootdir = HostHelpers::GetCJLSTSamplesDirectory(CJLSTdate);
 const TString CJLSTsamplesdir = CJLSTrootdir + "/" + CJLSTdate;

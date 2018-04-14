c693486a745c35fb41274082216d7b80bde661d5
Ulascan Sarica <ulascan90@gmail.com>
2018-04-14 08:36:34 -0400

    Change behavior of ExtendedProfileHistogram

--- Analysis/interface/ExtendedProfileHistogram.h
+++ Analysis/interface/ExtendedProfileHistogram.h
@@ -6,7 +6,8 @@
 
 
 class ExtendedProfileHistogram{
-  bool fillAverageXYZ;
+  // Can store up to 3 average quantities 
+  unsigned int fillAverageQ;
 
   ExtendedBinning xbinning;
   ExtendedBinning ybinning;
@@ -14,49 +15,38 @@ class ExtendedProfileHistogram{
 
   std::vector<std::vector<std::vector<double>>> sumW;
   std::vector<std::vector<std::vector<double>>> sumWsq;
-  std::vector<std::vector<std::vector<double>>> sumWX;
-  std::vector<std::vector<std::vector<double>>> sumWXsq;
-  std::vector<std::vector<std::vector<double>>> sumWY;
-  std::vector<std::vector<std::vector<double>>> sumWYsq;
-  std::vector<std::vector<std::vector<double>>> sumWZ;
-  std::vector<std::vector<std::vector<double>>> sumWZsq;
+  std::vector<std::vector<std::vector<double>>>* sumWQ;
+  std::vector<std::vector<std::vector<double>>>* sumWQsq;
 
 public:
-  ExtendedProfileHistogram(ExtendedBinning bX, bool fillAverageXYZ_);
-  ExtendedProfileHistogram(ExtendedBinning bX, ExtendedBinning bY, bool fillAverageXYZ_);
-  ExtendedProfileHistogram(ExtendedBinning bX, ExtendedBinning bY, ExtendedBinning bZ, bool fillAverageXYZ_);
+  ExtendedProfileHistogram(ExtendedBinning bX, unsigned int fillAverageQ_);
+  ExtendedProfileHistogram(ExtendedBinning bX, ExtendedBinning bY, unsigned int fillAverageQ_);
+  ExtendedProfileHistogram(ExtendedBinning bX, ExtendedBinning bY, ExtendedBinning bZ, unsigned int fillAverageQ_);
   ExtendedProfileHistogram(ExtendedProfileHistogram const& other);
+  ~ExtendedProfileHistogram();
 
   void fill(double x, double w);
   void fill(double x, double y, double w);
   void fill(double x, double y, double z, double w);
 
-  double getBinSumW(int ix, int iy=0, int iz=0);
-  double getBinSumWsq(int ix, int iy=0, int iz=0);
-  double getBinAvgX(int ix, int iy=0, int iz=0);
-  double getBinSigmaX(int ix, int iy=0, int iz=0);
-  double getBinAvgY(int ix, int iy=0, int iz=0);
-  double getBinSigmaY(int ix, int iy=0, int iz=0);
-  double getBinAvgZ(int ix, int iy=0, int iz=0);
-  double getBinSigmaZ(int ix, int iy=0, int iz=0);
+  void fillQ(unsigned int iQ, double x, double Q, double w);
+  void fillQ(unsigned int iQ, double x, double y, double Q, double w);
+  void fillQ(unsigned int iQ, double x, double y, double z, double Q, double w);
+
+  double getBinSumW(int ix, int iy=0, int iz=0) const;
+  double getBinSumWsq(int ix, int iy=0, int iz=0) const;
+  double getBinAvgQ(unsigned int iQ, int ix, int iy=0, int iz=0) const;
+  double getBinSigmaQ(unsigned int iQ, int ix, int iy=0, int iz=0) const;
 
   std::vector<std::vector<std::vector<double>>>& getSumWContainer(){ return sumW; }
   std::vector<std::vector<std::vector<double>>>& getSumWsqContainer(){ return sumWsq; }
-  std::vector<std::vector<std::vector<double>>>& getSumWXContainer(){ return sumWX; }
-  std::vector<std::vector<std::vector<double>>>& getSumWXsqContainer(){ return sumWXsq; }
-  std::vector<std::vector<std::vector<double>>>& getSumWYContainer(){ return sumWY; }
-  std::vector<std::vector<std::vector<double>>>& getSumWYsqContainer(){ return sumWYsq; }
-  std::vector<std::vector<std::vector<double>>>& getSumWZContainer(){ return sumWZ; }
-  std::vector<std::vector<std::vector<double>>>& getSumWZsqContainer(){ return sumWZsq; }
+  std::vector<std::vector<std::vector<double>>>& getSumWQContainer(unsigned int iQ);
+  std::vector<std::vector<std::vector<double>>>& getSumWQsqContainer(unsigned int iQ);
 
   std::vector<std::vector<std::vector<double>>> const& getSumWContainer() const{ return sumW; }
   std::vector<std::vector<std::vector<double>>> const& getSumWsqContainer() const{ return sumWsq; }
-  std::vector<std::vector<std::vector<double>>> const& getSumWXContainer() const{ return sumWX; }
-  std::vector<std::vector<std::vector<double>>> const& getSumWXsqContainer() const{ return sumWXsq; }
-  std::vector<std::vector<std::vector<double>>> const& getSumWYContainer() const{ return sumWY; }
-  std::vector<std::vector<std::vector<double>>> const& getSumWYsqContainer() const{ return sumWYsq; }
-  std::vector<std::vector<std::vector<double>>> const& getSumWZContainer() const{ return sumWZ; }
-  std::vector<std::vector<std::vector<double>>> const& getSumWZsqContainer() const{ return sumWZsq; }
+  std::vector<std::vector<std::vector<double>>> const& getSumWQContainer(unsigned int iQ) const;
+  std::vector<std::vector<std::vector<double>>> const& getSumWQsqContainer(unsigned int iQ) const;
 
 
 };
--- Analysis/src/ExtendedProfileHistogram.cc
+++ Analysis/src/ExtendedProfileHistogram.cc
@@ -6,111 +6,126 @@ using namespace std;
 using namespace HelperFunctions;
 
 
-ExtendedProfileHistogram::ExtendedProfileHistogram(ExtendedBinning bX, bool fillAverageXYZ_) : fillAverageXYZ(fillAverageXYZ_), xbinning(bX){
+ExtendedProfileHistogram::ExtendedProfileHistogram(ExtendedBinning bX, unsigned int fillAverageQ_) :
+  fillAverageQ(fillAverageQ_),
+  xbinning(bX),
+  sumWQ(nullptr), sumWQsq(nullptr)
+{
   sumW.assign(xbinning.getNbins(), std::vector<std::vector<double>>()); for (auto& v:sumW){ v.assign(1, std::vector<double>()); for (auto& vv:v) vv.assign(1, double(0.)); }
   sumWsq=sumW;
-  if (fillAverageXYZ){
-    sumWX=sumW;
-    sumWXsq=sumW;
-    sumWY=sumW;
-    sumWYsq=sumW;
-    sumWZ=sumW;
-    sumWZsq=sumW;
+  if (fillAverageQ>0){
+    sumWQ=new std::vector<std::vector<std::vector<double>>>[fillAverageQ];
+    sumWQsq=new std::vector<std::vector<std::vector<double>>>[fillAverageQ];
+    for (unsigned int iQ=0; iQ<fillAverageQ; iQ++){
+      sumWQ[iQ]=sumW;
+      sumWQsq[iQ]=sumW;
+    }
   }
 }
-ExtendedProfileHistogram::ExtendedProfileHistogram(ExtendedBinning bX, ExtendedBinning bY, bool fillAverageXYZ_) : fillAverageXYZ(fillAverageXYZ_), xbinning(bX), ybinning(bY){
+ExtendedProfileHistogram::ExtendedProfileHistogram(ExtendedBinning bX, ExtendedBinning bY, unsigned int fillAverageQ_) :
+  fillAverageQ(fillAverageQ_),
+  xbinning(bX), ybinning(bY),
+  sumWQ(nullptr), sumWQsq(nullptr)
+{
   sumW.assign(xbinning.getNbins(), std::vector<std::vector<double>>()); for (auto& v:sumW){ v.assign(ybinning.getNbins(), std::vector<double>()); for (auto& vv:v) vv.assign(1, double(0.)); }
   sumWsq=sumW;
-  if (fillAverageXYZ){
-    sumWX=sumW;
-    sumWXsq=sumW;
-    sumWY=sumW;
-    sumWYsq=sumW;
-    sumWZ=sumW;
-    sumWZsq=sumW;
+  if (fillAverageQ>0){
+    sumWQ=new std::vector<std::vector<std::vector<double>>>[fillAverageQ];
+    sumWQsq=new std::vector<std::vector<std::vector<double>>>[fillAverageQ];
+    for (unsigned int iQ=0; iQ<fillAverageQ; iQ++){
+      sumWQ[iQ]=sumW;
+      sumWQsq[iQ]=sumW;
+    }
   }
 }
-ExtendedProfileHistogram::ExtendedProfileHistogram(ExtendedBinning bX, ExtendedBinning bY, ExtendedBinning bZ, bool fillAverageXYZ_) : fillAverageXYZ(fillAverageXYZ_), xbinning(bX), ybinning(bY), zbinning(bZ){
+ExtendedProfileHistogram::ExtendedProfileHistogram(ExtendedBinning bX, ExtendedBinning bY, ExtendedBinning bZ, unsigned int fillAverageQ_) :
+  fillAverageQ(fillAverageQ_),
+  xbinning(bX), ybinning(bY), zbinning(bZ),
+  sumWQ(nullptr), sumWQsq(nullptr)
+{
   sumW.assign(xbinning.getNbins(), std::vector<std::vector<double>>()); for (auto& v:sumW){ v.assign(ybinning.getNbins(), std::vector<double>()); for (auto& vv:v) vv.assign(zbinning.getNbins(), double(0.)); }
   sumWsq=sumW;
-  if (fillAverageXYZ){
-    sumWX=sumW;
-    sumWXsq=sumW;
-    sumWY=sumW;
-    sumWYsq=sumW;
-    sumWZ=sumW;
-    sumWZsq=sumW;
+  if (fillAverageQ>0){
+    sumWQ=new std::vector<std::vector<std::vector<double>>>[fillAverageQ];
+    sumWQsq=new std::vector<std::vector<std::vector<double>>>[fillAverageQ];
+    for (unsigned int iQ=0; iQ<fillAverageQ; iQ++){
+      sumWQ[iQ]=sumW;
+      sumWQsq[iQ]=sumW;
+    }
   }
 }
 ExtendedProfileHistogram::ExtendedProfileHistogram(ExtendedProfileHistogram const& other) :
-  fillAverageXYZ(other.fillAverageXYZ),
+  fillAverageQ(other.fillAverageQ),
   xbinning(other.xbinning),
   ybinning(other.ybinning),
   zbinning(other.zbinning),
   sumW(other.sumW),
-  sumWsq(other.sumWsq),
-  sumWX(other.sumWX),
-  sumWXsq(other.sumWXsq),
-  sumWY(other.sumWY),
-  sumWYsq(other.sumWYsq),
-  sumWZ(other.sumWZ),
-  sumWZsq(other.sumWZsq)
-{}
+  sumWsq(other.sumWsq)
+{
+  if (fillAverageQ>0){
+    sumWQ=new std::vector<std::vector<std::vector<double>>>[fillAverageQ];
+    sumWQsq=new std::vector<std::vector<std::vector<double>>>[fillAverageQ];
+    for (unsigned int iQ=0; iQ<fillAverageQ; iQ++){
+      sumWQ[iQ]=other.sumWQ[iQ];
+      sumWQsq[iQ]=other.sumWQsq[iQ];
+    }
+  }
+}
+ExtendedProfileHistogram::~ExtendedProfileHistogram(){
+  if (fillAverageQ>0){
+    delete[] sumWQ;
+    delete[] sumWQsq;
+  }
+}
 
-double ExtendedProfileHistogram::getBinSumW(int ix, int iy, int iz){
+
+double ExtendedProfileHistogram::getBinSumW(int ix, int iy, int iz) const{
   if (ix<0 || (xbinning.isValid() && ix>=(int) xbinning.getNbins()) || (!xbinning.isValid() && ix>0)) return 0;
   if (iy<0 || (ybinning.isValid() && iy>=(int) ybinning.getNbins()) || (!ybinning.isValid() && iy>0)) return 0;
   if (iz<0 || (zbinning.isValid() && iz>=(int) zbinning.getNbins()) || (!zbinning.isValid() && iz>0)) return 0;
   return sumW.at(ix).at(iy).at(iz);
 }
-double ExtendedProfileHistogram::getBinSumWsq(int ix, int iy, int iz){
+double ExtendedProfileHistogram::getBinSumWsq(int ix, int iy, int iz) const{
   if (ix<0 || (xbinning.isValid() && ix>=(int) xbinning.getNbins()) || (!xbinning.isValid() && ix>0)) return 0;
   if (iy<0 || (ybinning.isValid() && iy>=(int) ybinning.getNbins()) || (!ybinning.isValid() && iy>0)) return 0;
   if (iz<0 || (zbinning.isValid() && iz>=(int) zbinning.getNbins()) || (!zbinning.isValid() && iz>0)) return 0;
   return sumWsq.at(ix).at(iy).at(iz);
 }
-double ExtendedProfileHistogram::getBinAvgX(int ix, int iy, int iz){
-  if (!fillAverageXYZ) return 0;
+double ExtendedProfileHistogram::getBinAvgQ(unsigned int iQ, int ix, int iy, int iz) const{
+  if (fillAverageQ==0 || iQ>=fillAverageQ) return 0;
   if (ix<0 || (xbinning.isValid() && ix>=(int) xbinning.getNbins()) || (!xbinning.isValid() && ix>0)) return 0;
   if (iy<0 || (ybinning.isValid() && iy>=(int) ybinning.getNbins()) || (!ybinning.isValid() && iy>0)) return 0;
   if (iz<0 || (zbinning.isValid() && iz>=(int) zbinning.getNbins()) || (!zbinning.isValid() && iz>0)) return 0;
-  return (sumW.at(ix).at(iy).at(iz)!=0. ? sumWX.at(ix).at(iy).at(iz) / sumW.at(ix).at(iy).at(iz) : 0.);
+  const std::vector<std::vector<std::vector<double>>>& sWQ = sumWQ[iQ];
+  return (sumW.at(ix).at(iy).at(iz)!=0. ? sWQ.at(ix).at(iy).at(iz) / sumW.at(ix).at(iy).at(iz) : 0.);
 }
-double ExtendedProfileHistogram::getBinSigmaX(int ix, int iy, int iz){
-  if (!fillAverageXYZ) return 0;
+double ExtendedProfileHistogram::getBinSigmaQ(unsigned int iQ, int ix, int iy, int iz) const{
+  if (fillAverageQ==0 || iQ>=fillAverageQ) return 0;
   if (ix<0 || (xbinning.isValid() && ix>=(int) xbinning.getNbins()) || (!xbinning.isValid() && ix>0)) return 0;
   if (iy<0 || (ybinning.isValid() && iy>=(int) ybinning.getNbins()) || (!ybinning.isValid() && iy>0)) return 0;
   if (iz<0 || (zbinning.isValid() && iz>=(int) zbinning.getNbins()) || (!zbinning.isValid() && iz>0)) return 0;
-  return sqrt(max(0., (sumW.at(ix).at(iy).at(iz)!=0. ? sumWXsq.at(ix).at(iy).at(iz) / sumW.at(ix).at(iy).at(iz) - pow(sumWX.at(ix).at(iy).at(iz) / sumW.at(ix).at(iy).at(iz), 2) : 0.)));
+  const std::vector<std::vector<std::vector<double>>>& sWQ = sumWQ[iQ];
+  const std::vector<std::vector<std::vector<double>>>& sWQsq = sumWQsq[iQ];
+  return sqrt(max(0., (sumW.at(ix).at(iy).at(iz)!=0. ? sWQsq.at(ix).at(iy).at(iz) / sumW.at(ix).at(iy).at(iz) - pow(sWQ.at(ix).at(iy).at(iz) / sumW.at(ix).at(iy).at(iz), 2) : 0.)));
 }
-double ExtendedProfileHistogram::getBinAvgY(int ix, int iy, int iz){
-  if (!fillAverageXYZ) return 0;
-  if (ix<0 || (xbinning.isValid() && ix>=(int) xbinning.getNbins()) || (!xbinning.isValid() && ix>0)) return 0;
-  if (iy<0 || (ybinning.isValid() && iy>=(int) ybinning.getNbins()) || (!ybinning.isValid() && iy>0)) return 0;
-  if (iz<0 || (zbinning.isValid() && iz>=(int) zbinning.getNbins()) || (!zbinning.isValid() && iz>0)) return 0;
-  return (sumW.at(ix).at(iy).at(iz)!=0. ? sumWY.at(ix).at(iy).at(iz) / sumW.at(ix).at(iy).at(iz) : 0.);
+
+std::vector<std::vector<std::vector<double>>>& ExtendedProfileHistogram::getSumWQContainer(unsigned int iQ){
+  assert(fillAverageQ!=0 && iQ<fillAverageQ);
+  return sumWQ[iQ];
 }
-double ExtendedProfileHistogram::getBinSigmaY(int ix, int iy, int iz){
-  if (!fillAverageXYZ) return 0;
-  if (ix<0 || (xbinning.isValid() && ix>=(int) xbinning.getNbins()) || (!xbinning.isValid() && ix>0)) return 0;
-  if (iy<0 || (ybinning.isValid() && iy>=(int) ybinning.getNbins()) || (!ybinning.isValid() && iy>0)) return 0;
-  if (iz<0 || (zbinning.isValid() && iz>=(int) zbinning.getNbins()) || (!zbinning.isValid() && iz>0)) return 0;
-  return sqrt(max(0., (sumW.at(ix).at(iy).at(iz)!=0. ? sumWYsq.at(ix).at(iy).at(iz) / sumW.at(ix).at(iy).at(iz) - pow(sumWY.at(ix).at(iy).at(iz) / sumW.at(ix).at(iy).at(iz), 2) : 0.)));
+std::vector<std::vector<std::vector<double>>>& ExtendedProfileHistogram::getSumWQsqContainer(unsigned int iQ){
+  assert(fillAverageQ!=0 && iQ<fillAverageQ);
+  return sumWQsq[iQ];
 }
-double ExtendedProfileHistogram::getBinAvgZ(int ix, int iy, int iz){
-  if (!fillAverageXYZ) return 0;
-  if (ix<0 || (xbinning.isValid() && ix>=(int) xbinning.getNbins()) || (!xbinning.isValid() && ix>0)) return 0;
-  if (iy<0 || (ybinning.isValid() && iy>=(int) ybinning.getNbins()) || (!ybinning.isValid() && iy>0)) return 0;
-  if (iz<0 || (zbinning.isValid() && iz>=(int) zbinning.getNbins()) || (!zbinning.isValid() && iz>0)) return 0;
-  return (sumW.at(ix).at(iy).at(iz)!=0. ? sumWZ.at(ix).at(iy).at(iz) / sumW.at(ix).at(iy).at(iz) : 0.);
+std::vector<std::vector<std::vector<double>>> const& ExtendedProfileHistogram::getSumWQContainer(unsigned int iQ) const{
+  assert(fillAverageQ!=0 && iQ<fillAverageQ);
+  return sumWQ[iQ];
 }
-double ExtendedProfileHistogram::getBinSigmaZ(int ix, int iy, int iz){
-  if (!fillAverageXYZ) return 0;
-  if (ix<0 || (xbinning.isValid() && ix>=(int) xbinning.getNbins()) || (!xbinning.isValid() && ix>0)) return 0;
-  if (iy<0 || (ybinning.isValid() && iy>=(int) ybinning.getNbins()) || (!ybinning.isValid() && iy>0)) return 0;
-  if (iz<0 || (zbinning.isValid() && iz>=(int) zbinning.getNbins()) || (!zbinning.isValid() && iz>0)) return 0;
-  return sqrt(max(0., (sumW.at(ix).at(iy).at(iz)!=0. ? sumWZsq.at(ix).at(iy).at(iz) / sumW.at(ix).at(iy).at(iz) - pow(sumWZ.at(ix).at(iy).at(iz) / sumW.at(ix).at(iy).at(iz), 2) : 0.)));
+std::vector<std::vector<std::vector<double>>> const& ExtendedProfileHistogram::getSumWQsqContainer(unsigned int iQ) const{
+  assert(fillAverageQ!=0 && iQ<fillAverageQ);
+  return sumWQsq[iQ];
 }
+
 
 void ExtendedProfileHistogram::fill(double x, double w){
   int ix=0, iy=0, iz=0;
@@ -120,9 +135,6 @@ void ExtendedProfileHistogram::fill(double x, double w){
   if (zbinning.isValid()) return;
   sumW.at(ix).at(iy).at(iz)+=w;
   sumWsq.at(ix).at(iy).at(iz)+=w*w;
-  if (!fillAverageXYZ) return;
-  sumWX.at(ix).at(iy).at(iz)+=w*x;
-  sumWXsq.at(ix).at(iy).at(iz)+=w*x*x;
 }
 void ExtendedProfileHistogram::fill(double x, double y, double w){
   int ix=0, iy=0, iz=0;
@@ -133,11 +145,6 @@ void ExtendedProfileHistogram::fill(double x, double y, double w){
   if (zbinning.isValid()) return;
   sumW.at(ix).at(iy).at(iz)+=w;
   sumWsq.at(ix).at(iy).at(iz)+=w*w;
-  if (!fillAverageXYZ) return;
-  sumWX.at(ix).at(iy).at(iz)+=w*x;
-  sumWXsq.at(ix).at(iy).at(iz)+=w*x*x;
-  sumWY.at(ix).at(iy).at(iz)+=w*y;
-  sumWYsq.at(ix).at(iy).at(iz)+=w*y*y;
 }
 void ExtendedProfileHistogram::fill(double x, double y, double z, double w){
   int ix=0, iy=0, iz=0;
@@ -149,11 +156,42 @@ void ExtendedProfileHistogram::fill(double x, double y, double z, double w){
   else{ iz=zbinning.getBin(z); if (iz<0 || iz>=(int) zbinning.getNbins()) return; }
   sumW.at(ix).at(iy).at(iz)+=w;
   sumWsq.at(ix).at(iy).at(iz)+=w*w;
-  if (!fillAverageXYZ) return;
-  sumWX.at(ix).at(iy).at(iz)+=w*x;
-  sumWXsq.at(ix).at(iy).at(iz)+=w*x*x;
-  sumWY.at(ix).at(iy).at(iz)+=w*y;
-  sumWYsq.at(ix).at(iy).at(iz)+=w*y*y;
-  sumWZ.at(ix).at(iy).at(iz)+=w*z;
-  sumWZsq.at(ix).at(iy).at(iz)+=w*z*z;
+}
+
+
+void ExtendedProfileHistogram::fillQ(unsigned int iQ, double x, double Q, double w){
+  int ix=0, iy=0, iz=0;
+  if (!xbinning.isValid()) return;
+  else{ ix=xbinning.getBin(x); if (ix<0 || ix>=(int) xbinning.getNbins()) return; }
+  if (ybinning.isValid()) return;
+  if (zbinning.isValid()) return;
+  std::vector<std::vector<std::vector<double>>>& sWQ = sumWQ[iQ];
+  std::vector<std::vector<std::vector<double>>>& sWQsq = sumWQsq[iQ];
+  sWQ.at(ix).at(iy).at(iz)+=w*Q;
+  sWQsq.at(ix).at(iy).at(iz)+=w*Q*Q;
+}
+void ExtendedProfileHistogram::fillQ(unsigned int iQ, double x, double y, double Q, double w){
+  int ix=0, iy=0, iz=0;
+  if (!xbinning.isValid()) return;
+  else{ ix=xbinning.getBin(x); if (ix<0 || ix>=(int) xbinning.getNbins()) return; }
+  if (!ybinning.isValid()) return;
+  else{ iy=ybinning.getBin(y); if (iy<0 || iy>=(int) ybinning.getNbins()) return; }
+  if (zbinning.isValid()) return;
+  std::vector<std::vector<std::vector<double>>>& sWQ = sumWQ[iQ];
+  std::vector<std::vector<std::vector<double>>>& sWQsq = sumWQsq[iQ];
+  sWQ.at(ix).at(iy).at(iz)+=w*Q;
+  sWQsq.at(ix).at(iy).at(iz)+=w*Q*Q;
+}
+void ExtendedProfileHistogram::fillQ(unsigned int iQ, double x, double y, double z, double Q, double w){
+  int ix=0, iy=0, iz=0;
+  if (!xbinning.isValid()) return;
+  else{ ix=xbinning.getBin(x); if (ix<0 || ix>=(int) xbinning.getNbins()) return; }
+  if (!ybinning.isValid()) return;
+  else{ iy=ybinning.getBin(y); if (iy<0 || iy>=(int) ybinning.getNbins()) return; }
+  if (!zbinning.isValid()) return;
+  else{ iz=zbinning.getBin(z); if (iz<0 || iz>=(int) zbinning.getNbins()) return; }
+  std::vector<std::vector<std::vector<double>>>& sWQ = sumWQ[iQ];
+  std::vector<std::vector<std::vector<double>>>& sWQsq = sumWQsq[iQ];
+  sWQ.at(ix).at(iy).at(iz)+=w*Q;
+  sWQsq.at(ix).at(iy).at(iz)+=w*Q*Q;
 }

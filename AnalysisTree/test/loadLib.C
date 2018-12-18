3cc202bc03fa9e5502fbfc78dfb5c6951fc67390
Ulascan Sarica <ulascan90@gmail.com>
2018-12-17 19:02:03 -0800

    Update library

--- AnalysisTree/test/loadLib.C
+++ AnalysisTree/test/loadLib.C
@@ -2,7 +2,7 @@
   gSystem->Load("$CMSSW_BASE/src/ZZMatrixElement/MELA/test/loadMELA.C");
   gSystem->Load("$CMSSW_BASE/src/HiggsAnalysis/CombinedLimit/test/loadLib.C");
 
-  gSystem->AddIncludePath("-I$CMSSW_BASE/src/HiggsWidth_PostICHEP/Analysis/interface/");
-  gSystem->AddIncludePath("-I$CMSSW_BASE/src/HiggsWidth_PostICHEP/Analysis/test/");
-  gSystem->Load("libHiggsWidth_PostICHEPAnalysis.so");
+  gSystem->AddIncludePath("-I$CMSSW_BASE/src/CMSDataTools/AnalysisTree/interface/");
+  gSystem->AddIncludePath("-I$CMSSW_BASE/src/CMSDataTools/AnalysisTree/test/");
+  gSystem->Load("libCMSDataToolsAnalysisTree.so");
 }

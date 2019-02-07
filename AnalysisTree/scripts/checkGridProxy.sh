e32dcc86ec51c90638b8a6bb493646a2b2063ada
Ulascan Sarica <ulascan90@gmail.com>
2019-02-07 05:50:29 -0800

    Add script to check proxy

--- AnalysisTree/scripts/checkGridProxy.sh
+++ AnalysisTree/scripts/checkGridProxy.sh
@@ -2,12 +2,12 @@
 
 USER_ID=$(id -u)
 proxy_valid=$(voms-proxy-info --timeleft)
-if [[ "$proxy_valid" == "" ]];then
+if [ -z $proxy_valid ];then
   voms-proxy-init --rfc --voms cms --hours 72 --valid 72:00
   proxy_valid=$(voms-proxy-info --timeleft)
 fi
 proxy_file="x509up_u$USER_ID"
-if [ $proxy_valid > 10 ];then
+if [ $proxy_valid -gt 10 ];then
    echo "GRID proxy found, validity: $proxy_valid s"
    if [ ! -z $X509_USER_PROXY ];then 
      if [[ "$X509_USER_PROXY" != "~/$proxy_file" ]];then
@@ -22,12 +22,12 @@ else # Last attempt: Try to see if a valid proxy already exists in ~
    if [ -e "~/$proxy_file" ];then
       export X509_USER_PROXY="~/$proxy_file"
       proxy_valid=${voms-proxy-info --timeleft}
-      if [ $proxy_valid > 10 ];then
+      if [ $proxy_valid -gt 10 ];then
          echo "GRID proxy found in ~, validity: $proxy_valid s"
       fi
    fi 
 
-   if [ $proxy_valid < 10 ];then
+   if [ $proxy_valid -lt 10 ];then
       echo "Error: no valid GRID proxy found."
       exit
    fi

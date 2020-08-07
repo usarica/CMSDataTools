f3228afa5cba1a9530735ed32796476b31e8001e
Ulascan Sarica <ulascan90@gmail.com>
2020-08-07 05:20:29 -0700

    Increase time out threshold

--- AnalysisTree/scripts/copyFromCondorToSite.sh
+++ AnalysisTree/scripts/copyFromCondorToSite.sh
@@ -45,12 +45,12 @@ if [[ ! -z ${FILENAME} ]];then
 
   COPY_SRC="file://${INPUTDIR}/${FILENAME}"
   COPY_DEST="gsiftp://gftp.${OUTPUTSITE}${OUTPUTDIR}/${RENAMEFILE}"
-  echo "Running: env -i X509_USER_PROXY=${X509_USER_PROXY} gfal-copy -p -f -t 7200 --verbose --checksum ADLER32 ${COPY_SRC} ${COPY_DEST}"
-  env -i X509_USER_PROXY=${X509_USER_PROXY} gfal-copy -p -f -t 7200 --verbose --checksum ADLER32 ${COPY_SRC} ${COPY_DEST}
+  echo "Running: env -i X509_USER_PROXY=${X509_USER_PROXY} gfal-copy -p -f -t 14400 --verbose --checksum ADLER32 ${COPY_SRC} ${COPY_DEST}"
+  env -i X509_USER_PROXY=${X509_USER_PROXY} gfal-copy -p -f -t 14400 --verbose --checksum ADLER32 ${COPY_SRC} ${COPY_DEST}
   COPY_STATUS=$?
   if [[ $COPY_STATUS != 0 ]]; then
     echo "Removing output file because gfal-copy crashed with code $COPY_STATUS"
-    env -i X509_USER_PROXY=${X509_USER_PROXY} gfal-rm -t 7200 --verbose ${COPY_DEST}
+    env -i X509_USER_PROXY=${X509_USER_PROXY} gfal-rm -t 14400 --verbose ${COPY_DEST}
     REMOVE_STATUS=$?
     if [[ $REMOVE_STATUS != 0 ]]; then
         echo "gfal-copy crashed and then the gfal-rm also crashed with code $REMOVE_STATUS"
